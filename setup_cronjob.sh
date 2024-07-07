9b624a8ec7101800d91fdd298d3fe9195505a058
Raven <130600806+Ravencrypt@users.noreply.github.com>
2024-07-07 17:32:00 +0200

    Update setup_cronjob.sh

--- setup_cronjob.sh
+++ setup_cronjob.sh
@@ -7,6 +7,8 @@ bash -c "echo 'docker restart satorineuron' >> $RESTART_SCRIPT"
 bash -c "echo 'sleep 300' >> $RESTART_SCRIPT"
 bash -c "echo 'systemctl restart satori.service' >> $RESTART_SCRIPT"
 chmod +x $RESTART_SCRIPT
-(crontab -l 2>/dev/null; echo '0 18 * * * /bin/bash /usr/local/bin/restart_satorineuron_and_service.sh') | crontab -') | crontab -
+
+# Set up the cron job to run the script at 6:00 PM every day
+(crontab -l 2>/dev/null; echo '0 18 * * * /bin/bash /usr/local/bin/restart_satorineuron_and_service.sh') | crontab -
 
 echo "Cron job setup completed successfully."
