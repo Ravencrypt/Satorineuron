1e10746c2691b73f06c89b5321a07b997699eec9
Raven <130600806+Ravencrypt@users.noreply.github.com>
2024-07-17 22:12:22 +0200

    Update setup_cronjob.sh

--- setup_cronjob.sh
+++ setup_cronjob.sh
@@ -3,9 +3,7 @@
 # Create Cronjob to restart Docker & Service
 RESTART_SCRIPT="/usr/local/bin/restart_satorineuron_and_service.sh"
 sudo bash -c "echo '#!/bin/bash' > $RESTART_SCRIPT"
-sudo bash -c "echo 'docker restart satorineuron' >> $RESTART_SCRIPT"
-sudo bash -c "echo 'sleep 300' >> $RESTART_SCRIPT"
-sudo bash -c "echo 'systemctl restart satori.service' >> $RESTART_SCRIPT"
+sudo bash -c "echo 'docker stop satorineuron' >> $RESTART_SCRIPT"
 sudo chmod +x $RESTART_SCRIPT
 
 # Set up the cron job to run the script at 6:00 PM every day
